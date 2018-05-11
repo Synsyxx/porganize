3dcd9368af3a840522c123d71751fd0277b74324
synsyxx <travis.simpson92@hotmail.com>
2018-05-11 10:56:48 -0500

    Add PersonalBookmark model

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 
-admin.site.register(Bookmark)
+admin.site.register((Bookmark, PersonalBookmark))
 
 # admin Porganize
