75ccb85e2faf53c598d1cbca0acb01636ab19aba
synsyxx <travis.simpson92@hotmail.com>
2018-05-14 16:12:21 -0500

    Added Notes Model

--- porganize/settings.py
+++ porganize/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'bootstrap4',
     'bookmarks',
+    'notes',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +73,15 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionOrAnonReadOnly'
+    ]
+}
+
 
 WSGI_APPLICATION = 'porganize.wsgi.application'
 
